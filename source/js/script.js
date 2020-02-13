502cc0d14d0abaf148f8ee219378c77e54f97e86
WinterInside <de_animator@mail.ru>
2020-02-13 07:55:42 +0300

    layout musician

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,20 @@
 var menu = document.querySelector('.header__menu');
-var nav = document.querySelector('.site-navigation');
+var nav = document.querySelector('.site-nav');
 
 menu.classList.remove('header__menu--nojs');
 menu.classList.add('header__menu--off');
-nav.classList.remove('site-navigation--nojs');
-nav.classList.add('site-navigation--closed');
+nav.classList.remove('site-nav--nojs');
+nav.classList.add('site-nav--closed');
 
 menu.addEventListener('click', function() {
-    if (nav.classList.contains('site-navigation--closed')) {
-    nav.classList.remove('site-navigation--closed');
-    nav.classList.add('site-navigation--opened');
+    if (nav.classList.contains('site-nav--closed')) {
+    nav.classList.remove('site-nav--closed');
+    nav.classList.add('site-nav--opened');
     menu.classList.add('header__menu--on');
     menu.classList.remove('header__menu--off');
   } else {
-    nav.classList.remove('site-navigation--opened');
-    nav.classList.add('site-navigation--closed');
+    nav.classList.remove('site-nav--opened');
+    nav.classList.add('site-nav--closed');
     menu.classList.remove('header__menu--on');
     menu.classList.add('header__menu--off');
   }
